afb2e38ad20c8884952544434849563152d7c0fc
aliy0012 <alikamal415@yahoo.com>
2021-10-04 16:38:11 +0000

    how many hundreds calculator function

---
+++ lighthouse-js-fundamentals/howManyHundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(numb){
+  let x = Math.trunc(numb / 100);
+  
+  return x;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
